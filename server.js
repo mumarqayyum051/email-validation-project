ccd6db0aa6bca93fcaca796011fdfdc2c88feb68
mumar <mumarqayyum051@gmail.com>
2021-12-17 02:26:17 +0500

    change app name

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/EMail"));
+app.use(express.static("./dist/erick-brandao"));
 
 app.get("/*", (req, res) =>
   res.sendFile("index.html", { root: "dist/angular-app-heroku/" })
