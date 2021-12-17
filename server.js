4b364e86212f45f28660d5abd582c81c11df4d7a
mumar <mumarqayyum051@gmail.com>
2021-12-17 10:43:06 +0500

    changes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/erick-brandao"));
+app.use(express.static("./dist/email-validation-project"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/erick-brandao/" })
+  res.sendFile("index.html", { root: "dist/email-validation-project/" })
 );
 
 // Start the app by listening on the default Heroku port
